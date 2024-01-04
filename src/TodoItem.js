143fd5e78abd50c601685dd7b8a8c96b74346eb0
hamaadasif <xhamaad@gmail.com>
2024-01-04 16:31:18 -0500

    basic check, name and delete

---
+++ src/TodoItem.js
@@ -0,0 +1,23 @@
+import "./styles.css"
+
+export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
+  return (
+    <li className="todo-item">
+      <div className="todo-checkbox">
+        <input
+          id={`todo-check-${id}`}
+          type="checkbox"
+          checked={completed}
+          onChange={e => toggleTodo(id, e.target.checked)}
+        />
+        <label htmlFor={`todo-check-${id}`}></label>
+      </div>
+      <div className="todo-title">{title}</div>
+      <div className="todo-delete">
+        <button onClick={() => deleteTodo(id)} className="btn btn-danger">
+          Delete
+        </button>
+      </div>
+    </li>
+  );
+}
---
+++ src/TodoList.js
@@ -0,0 +1,20 @@
+import { TodoItem } from "./TodoItem"
+import "./styles.css"
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && "Nothing to do"}
+      {todos.map(todo => {
+        return (
+          <TodoItem
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        )
+      })}
+    </ul>
+  )
+}
